583bdee99f2b8e69b1c46140ecf35157ae542870
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-01 15:33:02 +0530

    number printing

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+class Calculator{
+    constructor(previousOperandTextElm, currentOperandTextElm) {
+        this.previousOperandTextElm = previousOperandTextElm
+        this.currentOperandTextElm = currentOperandTextElm
+        this.clear()
+    }
+
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete(){}
+
+    appendNumber(number) {
+        this.currentOperand = number
+    }
+
+    chooseOperation(){}
+
+    compute() {}
+
+    updateDisplay() {
+        this.currentOperandTextElm.innerText = this.currentOperand
+    }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -5,3 +34,12 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElm = document.querySelector('[data-previous-operand]')
 const currentOperandTextElm = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElm, currentOperandTextElm)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
